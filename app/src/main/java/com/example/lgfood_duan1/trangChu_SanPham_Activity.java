8531452936cdbddc4d3d05240da2dbbecc62b22d
Vu Hieu Trung <vuhieutrung05102001@gmail.com>
2021-11-09 21:35:19 +0700

    zxfastớ
    tớ test code thằng nào pull về thì lỗi chetmebay

---
+++ app/src/main/java/com/example/lgfood_duan1/trangChu_SanPham_Activity.java
@@ -0,0 +1,1 @@
+package com.example.lgfood_duan1;import androidx.appcompat.app.AppCompatActivity;import android.os.Bundle;public class trangChu_SanPham_Activity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_trang_chu_san_pham);    }}
