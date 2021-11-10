14f5b544a7a7f92bb57d0645e119add3333d83f4
Vu Hieu Trung <vuhieutrung05102001@gmail.com>
2021-11-10 09:57:26 +0700

    thêm giao diện, trang chủ, thông tin tài khoản,item sản phẩm cuttom, thêm icon hình ảnh trong drable

---
+++ app/src/main/java/com/example/lgfood_duan1/thongTinTaiKhoan_Activity.java
@@ -0,0 +1,1 @@
+package com.example.lgfood_duan1;import androidx.appcompat.app.AppCompatActivity;import android.os.Bundle;public class thongTinTaiKhoan_Activity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_thong_tin_tai_khoan);    }}
