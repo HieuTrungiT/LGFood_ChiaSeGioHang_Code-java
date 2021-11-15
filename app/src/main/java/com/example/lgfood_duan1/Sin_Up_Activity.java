a1e7ab0e14fbaeee814b9a207b5905ac03ada812
QuangThaiii <93627034+QuangThaiii@users.noreply.github.com>
2021-11-15 15:52:22 +0700

    login normal add item custom thái

--- app/src/main/java/com/example/lgfood_duan1/Sin_Up_Activity.java
+++ app/src/main/java/com/example/lgfood_duan1/Sin_Up_Activity.java
@@ -3,6 +3,8 @@ package com.example.lgfood_duan1;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.Account;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +15,21 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Sin_Up_Activity extends AppCompatActivity {
 
@@ -36,20 +52,25 @@ public class Sin_Up_Activity extends AppCompatActivity {
             Login_tv_back,
             SingUp_Im_ViTri;
 //Login with google
-//    private static final String TAG = "GoogleActivity";
-//    private static final int RC_SIGN_IN = 9001;
-//    private FirebaseAuth mAuth;
-//    private GoogleSignInClient mGoogleSignInClient;
-//thai
-
+    private static final String TAG = "GoogleActivity";
+    private static final int RC_SIGN_IN = 9001;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+//Login normal
+    String reg = "^(0|\\+84)(\\s|\\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9])|(9[0-46-9]))(\\d)(\\d)(\\s|\\.)?(\\d{3})(\\s|\\.)?(\\d{3})?(\\d)$";
+    String emailPattern="[a-zA-z0-9._-]+@[gmail]+\\.+[com]+";
+    DatabaseReference node;
+    FirebaseDatabase database;
+    ProgressDialog progressDialog;
+    Account account;
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-//        FirebaseUser currentUser = mAuth.getCurrentUser();
-//        if (currentUser!=null){
-//            startActivity(new Intent(getApplicationContext(),MainActivity4.class));
-//        }
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (currentUser!=null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        }
     }
 
     @Override
@@ -58,69 +79,69 @@ public class Sin_Up_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_sin_up);
 
         anhXa();
-//        processrequest();
+        processrequest();
         batSuKien();
     }
 //khai báo client của google
-//    private void processrequest() {
-//        GoogleSignInOptions gso = new GoogleSignInOptions
-//                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                .requestIdToken(getString(R.string.default_web_client_id))
-//                .requestEmail()
-//                .build();
-//        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-//
-//    }
+    private void processrequest() {
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+    }
 //khi ấn nút login sẽ chạy vào hàm này
-//    private void processLogin() {
-//        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-//        startActivityForResult(signInIntent, RC_SIGN_IN);
-//    }
+    private void processLogin() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 //mở dialog tài khoản gg
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-//        if (requestCode == RC_SIGN_IN) {
-//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-//            handleSignInResult(task);
-//        }
-//    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
 //lay dữ liệu tài khoản
-//    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-//        try {
-//            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-//
-//            firebaseAuthWithGoogle(account.getIdToken());
-//
-//        } catch (ApiException e) {
-//            Toast.makeText(this, "fff", Toast.LENGTH_SHORT).show();
-//        }
-//    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            firebaseAuthWithGoogle(account.getIdToken());
+
+        } catch (ApiException e) {
+            Toast.makeText(this, "fail", Toast.LENGTH_SHORT).show();
+        }
+    }
 //xử lí đăng nhập
-//    private void firebaseAuthWithGoogle(String idToken) {
-//        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
-//        mAuth.signInWithCredential(credential)
-//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//                            // Sign in success, update UI with the signed-in user's information
-//                            Log.d(TAG, "signInWithCredential:success");
-//                            FirebaseUser user = mAuth.getCurrentUser();
-//                            if (user!=null){
-//                                startActivity(new Intent(getApplicationContext(),MainActivity4.class));
-//                            }
-//                        } else {
-//                            // If sign in fails, display a message to the user.
-//
-//                        }
-//
-//                        // ...
-//                    }
-//                });
-//    }
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if (user!=null){
+                                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Sin_Up_Activity.this, "fail", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
     private void batSuKien() {
 //        chuyển sang trang đăng kí tài khoản
         SignUp_tv_btn_Login.setOnClickListener(new View.OnClickListener() {
@@ -138,12 +159,12 @@ public class Sin_Up_Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-//        Login_tv_google.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-////                processLogin();
-//            }
-//        });
+        Login_tv_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                processLogin();
+            }
+        });
         Login_tv_Phone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -194,9 +215,12 @@ public class Sin_Up_Activity extends AppCompatActivity {
         SignUp_lv_btn_submid= findViewById(R.id.signUp_lv_btn_submid);
 
 //        Imgview
-        Login_tv_back = findViewById(R.id.login_tv_back);
+        Login_tv_back = findViewById(R.id.thongTinTaiKhoan_img_btn_thoat);
         SingUp_Im_ViTri = findViewById(R.id.singUp_Im_ViTri);
 //        Firebase auth
-//        mAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+//        Firebase realtime
+        database.getInstance("https://duan1-lgfood-default-rtdb.asia-southeast1.firebasedatabase.app/");
+
     }
 }
