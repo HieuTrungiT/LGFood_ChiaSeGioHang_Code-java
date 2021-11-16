fb9d58307ecf752d5ed5cc4b858ca326625b8b14
QuangThaiii <93627034+QuangThaiii@users.noreply.github.com>
2021-11-16 08:31:26 +0700

    login xong, thái

--- app/src/main/java/com/example/lgfood_duan1/Model/model_Account.java
+++ app/src/main/java/com/example/lgfood_duan1/Model/model_Account.java
@@ -12,4 +12,79 @@ public class model_Account {
     public model_Account(){
 
     }
+
+    public model_Account(String id, String name, String password, String address, String email, String phoneNumber, String idGioHang, String anhKhachHang) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.idGioHang = idGioHang;
+        this.anhKhachHang = anhKhachHang;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getIdGioHang() {
+        return idGioHang;
+    }
+
+    public void setIdGioHang(String idGioHang) {
+        this.idGioHang = idGioHang;
+    }
+
+    public String getAnhKhachHang() {
+        return anhKhachHang;
+    }
+
+    public void setAnhKhachHang(String anhKhachHang) {
+        this.anhKhachHang = anhKhachHang;
+    }
 }
