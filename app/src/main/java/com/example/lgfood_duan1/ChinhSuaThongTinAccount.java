f518243a093b206124b08b43da3a59f4a88e6169
QuangThaiii <93627034+QuangThaiii@users.noreply.github.com>
2021-11-16 10:23:18 +0700

    add activity_chinh_sua_thong_tin_account.xml
    thai dep dai

---
+++ app/src/main/java/com/example/lgfood_duan1/ChinhSuaThongTinAccount.java
@@ -0,0 +1,14 @@
+package com.example.lgfood_duan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChinhSuaThongTinAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chinh_sua_thong_tin_account);
+    }
+}
